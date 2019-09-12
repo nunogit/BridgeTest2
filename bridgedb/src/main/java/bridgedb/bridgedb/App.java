1f2d2067019421ace8743b662a7a6103ffb9fccd
nunogit <nunomsilvanunes@gmail.com>
2019-09-12 19:41:43 +0200

    bridgedb

---
+++ bridgedb/src/main/java/bridgedb/bridgedb/App.java
@@ -0,0 +1,18 @@
+package bridgedb.bridgedb;
+
+import org.bridgedb.Xref;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        //Xref xref = new Xref("X", "");
+        
+    }
+}
